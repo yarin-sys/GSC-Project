94bf761d1836a7b76665cf8014faf0a076aef7b9
VICKY3824D <pradana2766@gmail.com>
2025-05-07 07:47:46 +0700

    add profile

--- item-list/app.js
+++ item-list/app.js
@@ -34,7 +34,7 @@ fetch(endpoint, options)
 
         if (isValidData && data){
             let htmlStr = "";
-            let result_data = data
+            let result_data = data;
 
             for (result of result_data){
                 let rateString;
@@ -58,7 +58,9 @@ fetch(endpoint, options)
                     default:
                       // code block
                   }
-    
+                  
+                //   .toLocaleString("id-ID")
+
                 if (result.price_fina){
                     htmlStr +=  `
                     <div class="card mb-3 shadow-sm" id="card">
@@ -66,11 +68,11 @@ fetch(endpoint, options)
                             <h5 class="card-title">${result.item_name}</h5>
                             <img src="${result.picture}" class="card-img-top img-fluid mx-auto d-block mt-3" 
                             alt="${result.item_name}" style="width: 220px; height:180px; object-fit: cover;">
-                             <p class="card-text"><strong>Tingkat kerusakan:</strong> ${rateString} hari</p>
+                             <p class="card-text"><strong>Tingkat kerusakan:</strong> ${rateString} </p>
                             <p class="card-text"><strong>Deskripsi Kerusakan:</strong> ${result.deskripsi} ton</p>
                             <p class="card-text"><strong>Alamat:</strong> ${result.pick_address} ton</p>
-                            <p class="card-text"><strong>Harga penawaran :</strong> Rp${result.price_offered.toLocaleString("id-ID")}</p>
-                            <p class="card-text"><strong>Harga akhir :</strong> Rp${result.price_final.toLocaleString("id-ID")}</p>
+                            <p class="card-text"><strong>Harga penawaran :</strong> Rp${result.price_offered}</p>
+                            <p class="card-text"><strong>Harga akhir :</strong> Rp${result.price_final}</p>
                             <p class="card-text"><strong>Fixed:</strong> ${result.fixed}</p>
                             <br>
                         </div>
@@ -86,8 +88,8 @@ fetch(endpoint, options)
                              <p class="card-text"><strong>Tingkat kerusakan:</strong> ${rateString} </p>
                             <p class="card-text"><strong>Deskripsi Kerusakan:</strong> ${result.deskripsi}</p>
                             <p class="card-text"><strong>Alamat:</strong> ${result.pick_address}</p>
-                            <p class="card-text"><strong>Harga penawaran :</strong> Rp${result.price_offered.toLocaleString("id-ID")}</p>
-                            <p class="card-text"><strong>Harga akhir :</strong> Rp${result.price_final.toLocaleString("id-ID")}</p>
+                            <p class="card-text"><strong>Harga penawaran :</strong> Rp${result.price_offered}</p>
+                            <p class="card-text"><strong>Harga akhir :</strong> Rp${result.price_final}</p>
                             <p class="card-text"><strong>Fixed:</strong> ${result.fixed}</p>
                             <br>
                         </div>
@@ -95,7 +97,7 @@ fetch(endpoint, options)
                 `;
                 }
             }     
-            contentContainer.innerHTML = htmlStr
+            contentContainer.innerHTML = htmlStr;
             if (!data[0]){
                 contentContainer.innerHTML = "<p> Tidak ada Items </p>"
             }
---
+++ profile/app.js
@@ -0,0 +1,57 @@
+import { jwtDecode } from "https://cdn.jsdelivr.net/npm/jwt-decode@4.0.0/build/esm/index.js";
+
+function isTokenNotValid(jsonData) {
+    if(jsonData.code && jsonData.code === 'token_not_valid'){
+        // run a refresh token query
+
+        alert("Please Login Again");
+        window.location.href = "http://127.0.0.1:5500/signup-login/login/index.html";
+        return false;
+    }
+    return true;
+}
+
+const profileContainer = document.getElementById('profile-container');
+
+const authToken = localStorage.getItem("access");
+if (authToken) {
+  const decoded = jwtDecode(authToken);
+  const userId = decoded.user_id; // ini kuncinya
+  const userId_int = parseInt(userId)
+  console.log("User ID:", userId_int);
+
+  // Contoh fetch ke endpoint user
+  fetch(`http://127.0.0.1:8000/user/${userId_int}/`, {
+    method: "GET",
+    headers: {
+      "Authorization": `Bearer ${authToken}`,
+    }
+  })
+  .then(res => res.json())
+  .then(data => {
+    console.log("User detail:", data);
+    const isValidData = isTokenNotValid(data);
+    if (isValidData && data){
+        let htmlStr = "";
+        htmlStr +=  `
+        <div class="card mb-3 shadow-sm" id="card">
+            <div class="card-body">
+                <img src="${data.profile_pict}" class="card-img-top img-fluid mx-auto d-block mt-3" 
+                alt="${data.username}" style="width: 100px; height:100px; object-fit: cover;">
+                <h5 class="card-title">${data.username}</h5>
+                 <p class="card-text"><strong>Alamat:</strong> ${data.address} </p>
+                <p class="card-text"><strong>No Telepon:</strong> ${data.phone} </p>
+            </div>
+        </div>
+        `;
+        profileContainer.innerHTML = htmlStr;
+
+    }
+
+  })
+  .catch(err => {
+    console.error("Gagal mengambil data user:", err);
+  });
+} else {
+  console.error("Token tidak ditemukan di localStorage");
+}
