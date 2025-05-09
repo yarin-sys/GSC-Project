d502ccdde41ff41a78ec7a986a74008a734cccae
hanan <hananfijananto@gmail.com>
2025-05-09 16:26:22 +0700

    search-bar

--- item-list/app.js
+++ item-list/app.js
@@ -1,62 +1,61 @@
-const contentContainer = document.getElementById('content-container');
-const searchForm = document.getElementById('searchForm');
+const contentContainer = document.getElementById("content-container");
 
 const baseEndpoint = "http://localhost:8000";
 
-if(searchForm){
-    searchForm.addEventListener("submit", handleSearch);
+if (searchForm) {
+  searchForm.addEventListener("submit", handleSearch);
 }
 
 function isTokenNotValid(jsonData) {
-    if(jsonData.code && jsonData.code === 'token_not_valid'){
-        // run a refresh token query
-        alert("Please Login Again");
-        window.location.href = "http://127.0.0.1:5500/signup-login/login/index.html";
-        return false;
-    }
-    return true;
+  if (jsonData.code && jsonData.code === "token_not_valid") {
+    // run a refresh token query
+    alert("Please Login Again");
+    window.location.href = "http://127.0.0.1:5500/signup-login/login/index.html";
+    return false;
+  }
+  return true;
 }
 
 const endpoint = `${baseEndpoint}/items/`;
 
 function showData(data) {
-        console.log(data);
-        const isValidData = isTokenNotValid(data);
+  console.log(data);
+  const isValidData = isTokenNotValid(data);
 
-        if (isValidData && data){
-            let htmlStr = "";
-            let result_data = data;
+  if (isValidData && data) {
+    let htmlStr = "";
+    let result_data = data;
 
-            for (result of result_data){
-                let rateString;
+    for (result of result_data) {
+      let rateString;
 
-                switch(result.rate) {
-                    case 1:
-                        rateString = "Low";
-                        break;
-                    case 2:
-                        rateString = "Moderate";
-                        break;
-                    case 3:
-                        rateString = "Considerable";
-                        break;
-                    case 4:
-                        rateString = "Dangerous";
-                        break;
-                    case 4:
-                        rateString = "Considerable";
-                        break;
-                    default:
-                      // code block
-                  }
-                  
-                //   .toLocaleString("id-ID")
+      switch (result.rate) {
+        case 1:
+          rateString = "Low";
+          break;
+        case 2:
+          rateString = "Moderate";
+          break;
+        case 3:
+          rateString = "Considerable";
+          break;
+        case 4:
+          rateString = "Dangerous";
+          break;
+        case 4:
+          rateString = "Considerable";
+          break;
+        default:
+        // code block
+      }
 
-                // Owner & Harga akhir
-                let ownerInfo = result.owner ? `<p class="card-text"><strong>Owner:</strong> ${result.owner.username}</p>` : "";
-                let finalPriceInfo = result.price_final ? `<p class="card-text"><strong>Harga akhir:</strong> Rp${result.price_final.toLocaleString("id-ID")}</p>` : ""; 
+      //   .toLocaleString("id-ID")
 
-                htmlStr +=  `
+      // Owner & Harga akhir
+      let ownerInfo = result.owner ? `<p class="card-text"><strong>Owner:</strong> ${result.owner.username}</p>` : "";
+      let finalPriceInfo = result.price_final ? `<p class="card-text"><strong>Harga akhir:</strong> Rp${result.price_final.toLocaleString("id-ID")}</p>` : "";
+
+      htmlStr += `
                 <div class="card mb-3 shadow-sm" id="card">
                     <div class="card-body">
                         <h5 class="card-title">${result.item_name}</h5>
@@ -73,63 +72,74 @@ function showData(data) {
                     </div>
                 </div>
                 `;
-
-
-
-            }     
-            contentContainer.innerHTML = htmlStr;
-            if (!data[0]){
-                contentContainer.innerHTML = "<p> Tidak ada Items </p>"
-            }
-        } else {
-            contentContainer.innerHTML = "<p>Tidak ada Items </p>"
-        }
+    }
+    contentContainer.innerHTML = htmlStr;
+    if (!data[0]) {
+      contentContainer.innerHTML = "<p> Tidak ada Items </p>";
+    }
+  } else {
+    contentContainer.innerHTML = "<p>Tidak ada Items </p>";
+  }
 }
 
+function handleSearch(e) {
+  e.preventDefault();
 
-function handleSearch(e){
-    e.preventDefault();
+  let formData = new FormData(searchForm);
+  let data = Object.fromEntries(formData);
+  let searchParams = new URLSearchParams(data);
+  const searchItems = `${baseEndpoint}/search/item/?${searchParams}`;
 
-    let formData = new FormData(searchForm);
-    let data = Object.fromEntries(formData);
-    let searchParams = new URLSearchParams(data);
-    const searchItems = `${baseEndpoint}/search/item/?${searchParams}`;
+  const authToken = localStorage.getItem("access");
 
-    const authToken = localStorage.getItem('access');
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${authToken}`,
+    },
+  };
 
-    const options = {
-        method: "GET",
-        headers: {
-            "Authorization" : `Bearer ${authToken}`
-        }
-    };
-
-    fetch(searchItems, options)
+  fetch(searchItems, options)
     .then((response) => {
-        console.log("respon sebagai berikut: ",response)
-        return response.json()
+      console.log("respon sebagai berikut: ", response);
+      return response.json();
     })
-    .then(data => showData(data))
-    .catch((error) =>{
-        console.log(error);
+    .then((data) => showData(data))
+    .catch((error) => {
+      console.log(error);
     });
 }
 
-const authToken = localStorage.getItem('access');
+const authToken = localStorage.getItem("access");
 
 const options = {
-    method: "GET",
-    headers: {
-        "Authorization" : `Bearer ${authToken}`
-    }
+  method: "GET",
+  headers: {
+    Authorization: `Bearer ${authToken}`,
+  },
 };
 
 fetch(endpoint, options)
-    .then((response) => {
-        console.log("respon sebagai berikut: ",response)
-        return response.json()
-    })
-    .then(data => showData(data))
-    .catch((error) =>{
-        console.log(error);
+  .then((response) => {
+    console.log("respon sebagai berikut: ", response);
+    return response.json();
+  })
+  .then((data) => showData(data))
+  .catch((error) => {
+    console.log(error);
+  });
+
+const searchIcon = document.getElementById("search-icon");
+const searchForm = document.getElementById("search-form");
+const input = searchForm.querySelector('input[type="text"]');
+const button = searchForm.querySelector('input[type="submit"]');
+
+searchIcon.addEventListener("click", function () {
+  const isHidden = input.style.display === "none";
+  input.style.display = isHidden ? "inline-block" : "none";
+  button.style.display = isHidden ? "inline-block" : "none";
+
+  if (isHidden) {
+    input.focus();
+  }
 });
