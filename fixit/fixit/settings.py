f6a3c9c5f07902c8f5e4e22c4954676ea43cf1aa
VICKY3824D <pradana2766@gmail.com>
2025-05-15 06:22:22 +0700

    resolve dotenv

--- fixit/fixit/settings.py
+++ fixit/fixit/settings.py
@@ -12,8 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
 import datetime
 from django.contrib.messages import constants as messages
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
